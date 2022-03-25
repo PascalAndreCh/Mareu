bf015e244a0716613887e7a599cf91f7787bf328
PascalAndréCh <pascalandre.chenel@gmail.com>
2022-03-25 11:30:06 +0100

    ecriture layout et methode

--- app/src/main/java/com/projet4/maru/ui/meeting/MainActivity.java
+++ app/src/main/java/com/projet4/maru/ui/meeting/MainActivity.java
@@ -1,25 +1,132 @@
 package com.projet4.maru.ui.meeting;
 
+import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.DatePicker;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
 import com.projet4.maru.R;
+import com.projet4.maru.databinding.ActivityMainBinding;
 import com.projet4.maru.di.DI;
+import com.projet4.maru.model.Meeting;
+import com.projet4.maru.service.DummyMaReuGenerator;
 import com.projet4.maru.service.MaReuApiService;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collection;
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private MaReuApiService mApiService;
+    private ActivityMainBinding binding;
+    private List<Meeting> mMeetingArrayList = new ArrayList<>();
+    private Meeting mMeeting = (Meeting) DI.getStartListApiService().getMeetings();
+
+    private void initUI() {
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        setButton();
+        initRecyclerView();
+    }
+
+    private void initRecyclerView() {
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        binding.recyclerview.setLayoutManager(layoutManager);
+
+        MyMeetingRecyclerViewAdapter mAdapter = new MyMeetingRecyclerViewAdapter(mMeetingArrayList);
+        // Set CustomAdapter as the adapter for RecyclerView.
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(binding.recyclerview.getContext(),
+                layoutManager.getOrientation());
+        binding.recyclerview.addItemDecoration(dividerItemDecoration);
+        binding.recyclerview.setAdapter(mAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.filter_date:
+                dateDialog();
+                return true;
+            case R.id.filter_reset:
+                resetFilter();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void resetFilter() {
+        mMeetingArrayList.clear();
+        mMeetingArrayList.addAll((Collection<? extends Meeting>) mMeeting);
+        binding.recyclerview.getAdapter().notifyDataSetChanged();
+    }
+    private void dateDialog() {
+        int selectedYear = 2022;
+        int selectedMonth = 4;
+        int selectedDayOfMonth = 1;
+
+
+    DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+
+        @Override
+        public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+            Calendar cal = Calendar.getInstance();
+            cal.set(i, i1, i2);
+            mMeetingArrayList.clear();
+            mMeetingArrayList.addAll(mMeetingArrayList.getMeetingsByDate(cal.getTime()));
+            binding.recyclerview.getAdapter().notifyDataSetChanged();
+        }
+
+    };
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                dateSetListener, selectedYear, selectedMonth, selectedDayOfMonth);
+
+        datePickerDialog.show();
+    }
+
+    private void initData() {
+        mMeetingArrayList = new ArrayList<>(mMeetingArrayList.getMeetings());
+    }
+
+    private void setButton() {
+        binding.startAddActivity.setOnClickListener(this);
+    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        mApiService = DI.getStartListApiService(); // création des listes du jeu d'essai ???
+        initData();
+        initUI();
     }
 
     @Override
     public void onClick(View view) {
-
+        if (view == binding.startAddActivity) {
+            startActivity(new Intent(this, AddMeetingActivity.class));
+        }
     }
 }
--- app/src/main/java/com/projet4/maru/ui/meeting/MyMeetingRecyclerViewAdapter.java
+++ app/src/main/java/com/projet4/maru/ui/meeting/MyMeetingRecyclerViewAdapter.java
@@ -1,4 +1,63 @@
 package com.projet4.maru.ui.meeting;
 
-public class MyMeetingRecyclerViewAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.projet4.maru.R;
+import com.projet4.maru.model.Meeting;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyMeetingRecyclerViewAdapter extends RecyclerView.Adapter<MyMeetingRecyclerViewAdapter.ViewHolder> {
+
+        private final List<Meeting> meetings;
+
+        public MyMeetingRecyclerViewAdapter(ArrayList<Meeting> meetings) {
+                this.meetings = meetings;
+        }
+
+        @NonNull
+        @Override
+        public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.item_meeting, parent, false);
+                return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder viewHolder, final int position) {
+                Meeting meeting = meetings.get(position);
+                viewHolder.displayMeeting(meeting);
+        }
+
+        @Override
+        public int getItemCount() {
+                return meetings.size();
+        }
+
+        public static class ViewHolder extends RecyclerView.ViewHolder {
+                public final TextView meetingText;
+                public final TextView mails;
+
+                public ViewHolder(View view) {
+                        super(view);
+                        meetingText = view.findViewById(R.id.meetingText);
+                        mails = view.findViewById(R.id.mails);
+                }
+
+                public void displayMeeting(Meeting meeting) {
+                        SimpleDateFormat fmtOut = new SimpleDateFormat("dd-MM-yyyy hh:mm");
+
+                        meetingText.setText(meeting.getTitle()+" "+fmtOut.format(meeting.getTimeStart()+" "+meeting.getIdRoom()));
+                        mails.setText(meeting.getParticipants().getMailAddresses());
+                }
+        }
 }
+
